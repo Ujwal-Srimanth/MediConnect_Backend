6f890d1c41a06660f7c77438eaf738896c5bdf13
Ujwal-Srimanth <nusvarma@gmail.com>
2025-09-04 21:06:20 +0530

    logging

--- app/config.py
+++ app/config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+import logging
+
+logger = logging.getLogger("uvicorn.error")  # Uvicorn's default logger
+logger.setLevel(logging.INFO)
 
 load_dotenv()
 
@@ -11,10 +15,12 @@ ENV = os.getenv("ENV", "dev")
 MONGODB_URI = None
 DB_NAME = None
 JWT_SECRET = None
-print('hi there')
+logger.info('hi there')
 
 if ENV == "prod":
-    print("🔐 Using production environment variables from Azure Key Vault")
+    logger.info("🔐 Using production environment variables from Azure Key Vault")
+
+
 
     VAULT_URL = os.getenv("VAULT_URL") 
 
@@ -31,7 +37,7 @@ if ENV == "prod":
     ACS_CONNECTION_STRING = client.get_secret("ACSCONNECTIONSTRING").value
     SENDER_ADDRESS = client.get_secret("SENDERADDRESS").value
 
-    print(VAULT_URL, MONGODB_URI, DB_NAME, JWT_SECRET, API_BASE_URL, ACS_CONNECTION_STRING, SENDER_ADDRESS)
+    logger.info(VAULT_URL, MONGODB_URI, DB_NAME, JWT_SECRET, API_BASE_URL, ACS_CONNECTION_STRING, SENDER_ADDRESS)
    
 
 
@@ -41,7 +47,7 @@ if ENV == "prod":
     AZURE_CONTAINER_NAME = client.get_secret("AZURECONTAINERNAME").value
     AZURE_STORAGE_ACCOUNT = client.get_secret("AZURESTORAGEACCOUNT").value
     AZURE_STORAGE_KEY = client.get_secret("AZURESTORAGEKEY").value
-    print(AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY)
+    logger.info(AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY)
     blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
     container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
     try:
