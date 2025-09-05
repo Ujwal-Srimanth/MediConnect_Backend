8673b9340e6b9972a271aa774b7301d5561148af
Ujwal-Srimanth <nusvarma@gmail.com>
2025-09-05 11:51:01 +0530

    issue corrected

--- app/config.py
+++ app/config.py
@@ -3,24 +3,12 @@ from dotenv import load_dotenv
 
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-import logging
-
-logger = logging.getLogger("uvicorn.error")  # Uvicorn's default logger
-logger.setLevel(logging.INFO)
 
 load_dotenv()
 
 ENV = "prod"
 
-MONGODB_URI = None
-DB_NAME = None
-JWT_SECRET = None
-logger.info('hi there')
-
 if ENV == "prod":
-    logger.info("🔐 Using production environment variables from Azure Key Vault")
-
-
 
     VAULT_URL = os.getenv("VAULT_URL") 
 
@@ -36,8 +24,6 @@ if ENV == "prod":
     API_BASE_URL = client.get_secret("APIBASEURL").value
     ACS_CONNECTION_STRING = client.get_secret("ACSCONNECTIONSTRING").value
     SENDER_ADDRESS = client.get_secret("SENDERADDRESS").value
-
-    logger.info(VAULT_URL, MONGODB_URI, DB_NAME, JWT_SECRET, API_BASE_URL, ACS_CONNECTION_STRING, SENDER_ADDRESS)
    
 
 
@@ -47,7 +33,6 @@ if ENV == "prod":
     AZURE_CONTAINER_NAME = client.get_secret("AZURECONTAINERNAME").value
     AZURE_STORAGE_ACCOUNT = client.get_secret("AZURESTORAGEACCOUNT").value
     AZURE_STORAGE_KEY = client.get_secret("AZURESTORAGEKEY").value
-    logger.info(AZURE_STORAGE_CONNECTION_STRING, AZURE_CONTAINER_NAME, AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY)
     blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
     container_client = blob_service_client.get_container_client(AZURE_CONTAINER_NAME)
     try:
@@ -55,7 +40,5 @@ if ENV == "prod":
     except Exception:
         pass  
 
-
-# Uploads directory
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
